9e4a6da299e4cd0b2506bea8323bb33a504873cf
Ryan Allen <rallen090@gmail.com>
2017-07-10 01:51:41 -0400

    UI fixes for live tracking

--- Web/ClientApp/components/ContractInfo.tsx
+++ Web/ClientApp/components/ContractInfo.tsx
@@ -1,13 +1,14 @@
 ﻿import * as $ from 'jquery';
 import * as React from 'react';
 import { RouteComponentProps } from 'react-router-dom';
-import { Header } from 'semantic-ui-react'
+import { Header, Statistic, Progress, Label, Segment } from 'semantic-ui-react'
 
 interface IContractState {
 	isPastStartDate: boolean,
 	isPastEndDate: boolean,
 	crowdsaleAddress: string,
 	jobiAddress: string,
+	ethRaised: number,
 }
 
 export default class ContractInfo extends React.Component<{}, IContractState> {
@@ -27,7 +28,8 @@ export default class ContractInfo extends React.Component<{}, IContractState> {
 			isPastStartDate: hasStarted,
 			isPastEndDate: hasEnded,
 			crowdsaleAddress: null,
-			jobiAddress: null
+			jobiAddress: null,
+			ethRaised: null
 		}
 	}
 	timer() {
@@ -41,7 +43,8 @@ export default class ContractInfo extends React.Component<{}, IContractState> {
 				isPastStartDate: hasStarted,
 				isPastEndDate: hasEnded,
 				crowdsaleAddress: null,
-				jobiAddress: null
+				jobiAddress: null,
+				ethRaised: null
 			});	
 		}
 	}
@@ -53,9 +56,10 @@ export default class ContractInfo extends React.Component<{}, IContractState> {
 	}
 	isPastStartDate() {
 		// note: new Date() is default already UTC
-		var currentDate = new Date();
-		var msDiff = this.startDate.getTime() - currentDate.getTime();
-		return msDiff < 0;
+		//var currentDate = new Date();
+		//var msDiff = this.startDate.getTime() - currentDate.getTime();
+		//return msDiff < 0;
+		return true;
 	}
 	isPastEndDate() {
 		// note: new Date() is default already UTC
@@ -76,23 +80,62 @@ export default class ContractInfo extends React.Component<{}, IContractState> {
 				contentType: 'application/json; charset=utf-8',
 				dataType: 'json',
 				cache: false,
-				success: function (data) {
+				success: function(data) {
 					console.log(data);
 					if (data && data.crowdSaleAddress) {
-						this.pollTimeInMs = 30000;
 						this.setState({
 							isPastStartDate: this.state.isPastStartDate,
 							isPastEndDate: this.state.isPastEndDate,
 							crowdsaleAddress: data.crowdSaleAddress,
-							jobiAddress: data.jobiAddress
+							jobiAddress: data.jobiAddress,
+							ethRaised: this.state.ethRaised
 						});
 					}
 				}.bind(this),
-				error: function (xhr, status, err) {
+				error: function(xhr, status, err) {
 					console.error(status, err.toString());
 				}.bind(this)
 			});
+		} else {
+			this.getAmountRaised();
 		}
+	}
+	getAmountRaised() {
+		if (!this.state.crowdsaleAddress) {
+			return;
+		}
+
+		var apiKey = "UJ1PWQUVEDZ9MESTBIW6X3S57JJI4TH1CK";
+		var contractAddress = "0xaBE3d12e5518BF8266bB91B56913962ce1F77CF4"; // this.state.crowdsaleAddress
+		var url = `https://api.etherscan.io/api?module=account&action=balance&address=${contractAddress}&tag=latest&apikey=${apiKey}`;
+		
+		$.ajax({
+			method: "GET",
+			url: url,
+			cache: false,
+			success: function (data) {
+				if (data && data.result) {
+					this.pollTimeInMs = 10000;
+					var weiRaised = parseInt(data.result);
+					var ethRaised = weiRaised / 1000000000000000000;
+					this.setState({
+						isPastStartDate: this.state.isPastStartDate,
+						isPastEndDate: this.state.isPastEndDate,
+						crowdsaleAddress: this.state.crowdsaleAddress,
+						jobiAddress: data.jobiAddress,
+						ethRaised: ethRaised
+					});
+				}
+			}.bind(this),
+			error: function (xhr, status, err) {
+				console.error(status, err.toString());
+			}.bind(this)
+		});
+	}
+	numberWithCommas(x) {
+		var parts = x.toString().split('.');
+		var decimals = parts.length > 1 ? "." + parts[1] : "";
+		return parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",") + decimals;
 	}
 	render() {
 		return this.state.isPastStartDate ? (
@@ -119,7 +162,7 @@ export default class ContractInfo extends React.Component<{}, IContractState> {
 							</Header>
 							<Header icon textAlign='center' size='huge'>
 								<Header.Content>
-									CoinJob Crowdsale Address: {this.state.crowdsaleAddress ? (this.state.crowdsaleAddress) : "Not yet available"}
+									CoinJob Crowdsale Address: {this.state.crowdsaleAddress ? (this.state.crowdsaleAddress) : "Determining..."}
 								</Header.Content>
 								<Header.Subheader>
 									To participate in the ICO and purchase Jobis, send ETH to this contract address using the <a href="https://github.com/ethereum/mist/releases">Ethereum Wallet</a>.
@@ -129,7 +172,29 @@ export default class ContractInfo extends React.Component<{}, IContractState> {
 							</Header>
 						</div>
 					)}
-				<span></span>
+				{this.state.ethRaised
+					?
+					(
+						<div>
+							<Statistic.Group widths='2' items={[
+								{ label: 'ETH Raised', value: this.numberWithCommas(this.state.ethRaised.toFixed(5)) },
+								{ label: 'XCJ Sold (approximated)', value: `${this.numberWithCommas((this.state.ethRaised * 1000).toFixed(5))}` }
+							]} size='large' />
+							<Progress color="orange" percent={(this.state.ethRaised * 1000 / 20300000)} indicating />
+							<div className="progress-label-container">
+								<Label className="progress-label" size='big' basic color='orange' pointing>Progress to min funding goal of 20.3mm XCJ</Label>
+								<Label className="progress-label" size='big' basic color='red' pointing='below'>Progress to max funding goal of 60mm XCJ</Label>
+							</div>
+							<br/>
+							<Progress color="red" percent={(this.state.ethRaised * 1000 / 60000000)} indicating>
+							</Progress>
+						</div>
+					)
+					: 
+						<Statistic.Group widths='1' items={[
+							{ label: 'Loading live stats...' },
+						]} size='large' />
+					}
 			</div>
 		)
 		: <span></span>;
